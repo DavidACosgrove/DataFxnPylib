b4efdb5cb92c8536a514cbfdba4cad1c8e0e56fc
Me <david@cozchemix.co.uk>
2023-05-25 13:31:36 +0100

    Debugging.

--- pylib_3.9.7/df/replace_bioisostere_linkers.py
+++ pylib_3.9.7/df/replace_bioisostere_linkers.py
@@ -308,8 +308,11 @@ def fettled_linker_smis(linker_smi: str) -> list[str]:
     """
     out_smis = []
     for smi in linker_smi.split('.'):
-        new1, new2 = extract_dummy_atoms_from_smiles(smi)
-        out_smis.append(smi.replace(new1, '[*:1]').replace(new2, '[*:2]'))
+        if smi:
+            new1, new2 = extract_dummy_atoms_from_smiles(smi)
+            out_smis.append(smi.replace(new1, '[*:1]').replace(new2, '[*:2]'))
+        else:
+            out_smis.append('c1ccccc1')
 
     return out_smis
 
